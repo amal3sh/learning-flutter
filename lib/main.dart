9d2f21fbf1bfd8c6083f9687e468bdcf2f999d55
amal3sh <amaleshuk14@gmail.com>
2023-08-10 12:36:08 +0530

    feat(stateless widget)

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
-import './screens/welcome_screen.dart';
+import 'package:flutterbasics/screens/quiz.dart';
+// import './screens/welcome_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const Quiz());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168)
-              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-            ),
-            child: const WelcomeScreen()),
-      ),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Container(
+//             decoration: const BoxDecoration(
+//               gradient: LinearGradient(colors: [
+//                 Color.fromARGB(255, 78, 13, 151),
+//                 Color.fromARGB(255, 107, 15, 168)
+//               ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+//             ),
+//             child: const WelcomeScreen()),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/screens/questions_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreens extends StatefulWidget {
+  const QuestionsScreens({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreens> {
+  Widget build(BuildContext context) {
+    return const Text("Questions screen");
+  }
+}
